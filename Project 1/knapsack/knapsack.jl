8a5b161c81432d453b8422fed5755080029d7de7
Ola Johannes Elvedahl <olaelve5@outlook.com>
2026-01-29 15:01:23 +0100

    Moved all functions used in knapsack into main file to prepare for delivery

--- Project 1/knapsack/knapsack.jl
+++ Project 1/knapsack/knapsack.jl
@@ -4,15 +4,46 @@ using Random
 using Statistics
 using Plots
 
-include(joinpath(@__DIR__, "../common/fitness.jl"))
-include(joinpath(@__DIR__, "../common/individual.jl"))
-include(joinpath(@__DIR__, "../common/mutate.jl"))
-include(joinpath(@__DIR__, "../common/crossover.jl"))
-include(joinpath(@__DIR__, "../common/plot.jl"))
-include(joinpath(@__DIR__, "../common/survivor_selection.jl"))
+Random.seed!(123)
 
 file_path = joinpath(@__DIR__, "knapPI_12_500_1000_82.csv")
 df = CSV.read(file_path, DataFrame)
+
+
+# ============================================================================
+# GA Hyperparameters
+# I guess genes_size isn't really a hyperparameter, but it is defined here for convenience
+# ============================================================================
+const KNAPSACK_CAPACITY::Int64 = 280785
+const POPULATION_SIZE::Int64 = 500
+const GENES_SIZE::Int64 = 500
+const CROSSOVER_PROB::Float64 = 0.8
+const MUTATION_RATE::Float64 = 0.0001
+const MAX_GENERATIONS::Int64 = 10000
+
+
+# ============================================================================
+# Population initialization + individual object
+# ============================================================================
+
+mutable struct Individual
+    genes::BitVector
+    fitness::Float64
+    selection_prob::Float64
+end
+
+Individual(size::Int) = Individual(Random.bitrand(size), 0.0, 0.0)
+Individual(genes::BitVector) = Individual(genes, 0.0, 0.0)
+
+function initialize_population(pop_size, genes_size)
+    population = [Individual(genes_size) for _ in 1:pop_size]
+    return population
+end
+
+
+# ============================================================================
+# Fitness-calculation
+# ============================================================================
 
 const WEIGHTS = df.w
 const PRICES = df.p
@@ -62,7 +93,13 @@ function fitness_score(arr::BitVector)
     return max(0.1, final_score)
 end
 
+calculate_fitness_sum(population) = sum(ind.fitness for ind in population)
+mean_fitness(pop) = mean(ind.fitness for ind in pop)
 
+
+# ============================================================================
+# Parent pool selection + next-gen generation
+# ============================================================================
 """
 parent_selection() uses FPS with roulette wheel selection. 
 Returns a parent pool of the same size as the original population.
@@ -126,12 +163,134 @@ function generate_next_gen(parent_pool::Vector{Individual})
 end
 
 
+
+
+# ============================================================================
+# Crossover
+# ============================================================================
+"""
+Crossover function that combines two parent BitVectors to produce offspring
+based on a crossover probability.
+"""
+function crossover(parent1_vec::BitVector, parent2_vec::BitVector)
+    if rand() < CROSSOVER_PROB
+        cut_point = rand(1:GENES_SIZE)
+        child1 = vcat(parent1_vec[1:cut_point], parent2_vec[cut_point+1:end])
+        child2 = vcat(parent2_vec[1:cut_point], parent1_vec[cut_point+1:end])
+
+        return [child1, child2]
+    else
+        return (copy(parent1_vec), copy(parent2_vec))
+    end
+end
+
+
+# ============================================================================
+# Mutation
+# ============================================================================
+"""
+Mutates the genes of an individual BitVector in place based on the MUTATION_RATE
+"""
+function mutate!(genes::BitVector)
+    for i in eachindex(genes)
+        if rand() < MUTATION_RATE
+            genes[i] = 1 - genes[i]
+        end
+    end
+end
+
+
+# ============================================================================
+# Survivor selection
+# ============================================================================
+"""
+Combines the old population and children, then selects the top individuals based on fitness.
+The population returned is the new generation.
+"""
+function global_elitism(old_pop::Vector{Individual}, children::Vector{Individual})
+    pop_size = length(old_pop)
+    
+    combined_pool = vcat(old_pop, children)
+    sort!(combined_pool, by = ind -> ind.fitness, rev=true)
+    
+    return combined_pool[1:pop_size]
+end
+
+
+"""
+Returns a new population by keeping the elite individuals from the old population
+and filling the rest with the best individuals from the children.
+"""
+function elitism(old_pop::Vector{Individual}, children::Vector{Individual}, elite_count::Int)
+    sort!(old_pop, by = ind -> ind.fitness, rev=true)
+    elites = old_pop[1:elite_count]
+    
+    sort!(children, by = ind -> ind.fitness, rev=true)
+    non_elites = children[1:(length(children) - elite_count)]
+    
+    return vcat(elites, non_elites)
+end
+
+
+# ===========================================================================
+# Helper functions
+# ===========================================================================
 """
 Helper functions to get weight and check validity of a solution.
 """
 get_weight(genes::BitVector) = sum(WEIGHTS[i] for (i, bit) in enumerate(genes) if bit)
 valid_solution(weight::Int64) = KNAPSACK_CAPACITY - weight >= 0
 
+
+# ============================================================================
+# Plotting
+# ============================================================================
+"""
+plot_fitness() is only used in the knapsack problem. It plots the fitness scores
+over generations, highlighting the best score and generation if provided.
+"""
+function plot_fitness(
+    mean_scores, 
+    minimum_scores, 
+    maximum_scores, 
+    best_score=nothing, 
+    best_generation=nothing; 
+    outfile="Project 1/knapsack/fitness_plot.pdf")
+    
+    plot(mean_scores,
+         label="Mean Fitness",
+         xlabel="Generation",
+         ylabel="Fitness Score",
+         title="Genetic Algorithm Performance",
+         lw=2,
+         size=(800, 600))
+
+    plot!(maximum_scores, label="Max Fitness", color=:green)
+    plot!(minimum_scores, label="Min Fitness", color=:red)
+
+    if best_score !== nothing && best_generation !== nothing
+        best_score_label = best_score isa Integer ? string(best_score) : string(round(best_score, digits=3))
+        vline!([best_generation],
+               label="Best $(best_score_label) - gen $(best_generation)",
+               linestyle=:dash,
+               color=:black,
+               lw=2)
+
+        scatter!([best_generation], [best_score],
+                 label=false,
+                 markershape=:star5,
+                 color=:black)
+    end
+
+    savefig(outfile)
+    return nothing
+end
+
+
+
+# ============================================================================
+# Main GA loop
+# ============================================================================
 """
 main() runs the full genetic algorithm loop.
 It filters out invalid solutions when tracking the best fitness scores, but
@@ -197,17 +356,5 @@ function main()
     readline()
 end
 
-
-# Simulation Parameters
-const KNAPSACK_CAPACITY::Int64 = 280785
-const POPULATION_SIZE::Int64 = 500
-const GENES_SIZE::Int64 = 500
-const CROSSOVER_PROB::Float64 = 0.8
-const MUTATION_RATE::Float64 = 0.0001
-const MAX_GENERATIONS::Int64 = 10000
-
-
+# Run the genetic algorithm
 main()
-
-
-        
